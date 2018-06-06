8fe10f9a7dc19a78ca9ce482d3f97b6860153bdd
lin <nicky.lzx@gmail.com>
2018-06-06 12:19:20 +0800

    新增Aplayer

---
+++ assets/js/boo.js
@@ -0,0 +1,31 @@
+function load163Song(container){
+    var mid = container.getAttribute("mid");
+    fetch("http://music.huaji8.top/?id="+mid).then(function(res){return res.json()}).then(function(data){
+        const ap = new APlayer({
+            //container: document.getElementById(divId),
+            container : container,
+            lrcType: 1,
+            audio: [{
+                name: data.info.songs[0].name,
+                artist: data.info.songs[0].ar.map(function(ar){ return ar.name }).join(','),
+                url: data.url.url,
+                cover: data.pic.url,
+                lrc: data.lyric.lyric
+            }]
+        });
+    });
+}
+
+function initAllPlayer(){
+    let elements = document.getElementsByClassName("yyy");
+    for(let i=0;i<elements.length;i++){
+        load163Song(elements[i]);
+    }
+}
+
+!(function init(){
+    hljs.initHighlightingOnLoad();
+    initAllPlayer();
+})();
+
+
