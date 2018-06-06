2ad1a0e3b7a3d542e2b96c0b4795f94075903df8
lin <nicky.lzx@gmail.com>
2018-06-06 18:03:21 +0800

    meplayer

--- assets/js/boo.js
+++ assets/js/boo.js
@@ -1,29 +1,46 @@
-function load163Song(container){
-    var mid = container.getAttribute("mid");
-    var src = container.getAttribute("src");
-    fetch("http://music.huaji8.top/?id="+mid).then(function(res){return res.json()}).then(function(data){
-        const ap = new APlayer({
-            container : container,
-            lrcType: 1,
-            audio: [{
-                name: data.info.songs[0].name,
-                artist: data.info.songs[0].ar.map(function(ar){ return ar.name }).join(','),
-                url: src!=null?src:data.url.url,
+function load163Song(container) {
+    let mid = container.getAttribute("mid");
+    let src = container.getAttribute("src");
+    fetch("http://music.huaji8.top/?id=" + mid).then(function (res) {
+        return res.json()
+    }).then(function (data) {
+        // const ap = new APlayer({
+        //     container : container,
+        //     lrcType: 1,
+        //     audio: [{
+        //         name: data.info.songs[0].name,
+        //         artist: data.info.songs[0].ar.map(function(ar){ return ar.name }).join(','),
+        //         url: src!=null?src:data.url.url,
+        //         cover: data.pic.url,
+        //         lrc: data.lyric.lyric
+        //     }]
+        // });
+
+        const mp = new mePlayer({
+            target: container,
+            music: {
+                src: src != null ? src : data.url.url,
+                title: data.info.songs[0].name,
+                author: data.info.songs[0].ar.map(function (ar) {
+                    return ar.name
+                }).join(','),
                 cover: data.pic.url,
                 lrc: data.lyric.lyric
-            }]
+            },
+            autoplay: false // 是否自动播放
         });
+
     });
 }
 
-function initAllPlayer(){
+function initAllPlayer() {
     let elements = document.getElementsByClassName("yyy");
-    for(let i=0;i<elements.length;i++){
+    for (let i = 0; i < elements.length; i++) {
         load163Song(elements[i]);
     }
 }
 
-!(function init(){
+!(function init() {
     hljs.initHighlightingOnLoad();
     initAllPlayer();
 })();
